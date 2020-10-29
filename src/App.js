641ef7c95f16fd3da091238f83d72ae94bcd8403
Aman Oberoi <40956188+TheDarkLord247@users.noreply.github.com>
2020-10-29 21:23:59 +0530

    Added a background image and an About Me section (#1)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar.js';
 import SideDrawer from './components/SideDrawer/SideDrawer.js';
 import Backdrop from './components/Backdrop/Backdrop.js';
+import bg from './img/bg2.png';
 
 
 class App extends Component {
@@ -33,6 +34,20 @@ class App extends Component {
         <SideDrawer show={this.state.sideDrawerOpen}/>
         {backdrop}
         <main>
+          <div className="aboutme">
+            <img id="bg" src={bg} alt="Photograph of the night sky over an isolated, illuminated fort in Udaipur, India."></img>
+            <div className="centered">
+              <p className="intro">
+                My name is Aman Oberoi and I am a sophomore studying math and computer science at UCLA. 
+                My interests include algorithms, machine learning, software engineering and photography!
+              </p>
+            </div>
+          </div>
+          <div className="black">
+            <div className="card">
+              <h1>Yoda</h1>
+            </div>
+          </div>
         </main>
       </div>
     );
