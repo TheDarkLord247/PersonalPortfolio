5ec70e98a43a32c15f4f12f43b58170a67a06fdb
TheDarkLord247 <40956188+TheDarkLord247@users.noreply.github.com>
2020-10-31 16:34:05 +0530

    attempt #4

--- src/App.test.js
+++ src/App.test.js
@@ -2,3 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Aman/i);
+  expect(linkElement).toBeInTheDocument();
+});
