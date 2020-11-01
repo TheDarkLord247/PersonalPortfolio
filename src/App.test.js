9fa8efa99c8333b79139fb2f3b8c395bd4068136
TheDarkLord247 <40956188+TheDarkLord247@users.noreply.github.com>
2020-11-01 14:59:53 +0530

    Minor testing script error

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Aman/i);
+  const linkElement = getAllByText(/Aman/i);
   expect(linkElement).toBeInTheDocument();
 });
