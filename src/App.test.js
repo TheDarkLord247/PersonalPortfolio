a72388ddd424ad8041b89267337bf6e19cb2e345
TheDarkLord247 <40956188+TheDarkLord247@users.noreply.github.com>
2020-11-01 15:05:01 +0530

    attempt to fix react-scripts error

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getAllByText(/Aman/i);
+  const linkElement = getByText(/Udaipur/i);
   expect(linkElement).toBeInTheDocument();
 });
